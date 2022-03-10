eade18753e654861845822b2d887952508b86396
mnrx2020 <jamesmbere@gmail.com>
2022-03-10 10:39:27 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,13 +3,16 @@
 /**
  * main - Entry point
  *
+ * Description: prints "with proper grammar, but the outcome is a piece of
+ * art,"
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 
 {
 
-		puts("\"Programming is like building a multilingual puzzle");
+		printf("with proper grammar, but the outcome is a piece of art,\n");
 
 			return (0);
 
