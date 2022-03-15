263289aeffb0b9c1087aa2340c96605ab380de7c
mnrx2020 <jamesmbere@gmail.com>
2022-03-15 07:55:07 -0700

    Next level coding

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,14 +1,18 @@
-#include <main.h>
-
+#include "main.h"
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * main - print _putchar followed by a new line
+ * Return: Always 0 Success)
  */
-int _putchar(char c)
+int main (void)
 
 {
-	return (write(1, &c, 1));
+	char *ch = "_putchar";
+
+	while (*ch)
+	{
+		_putchar(*ch);
+		ch++;
+	}
+	_putchar('\n);
+	return (0);
 }
