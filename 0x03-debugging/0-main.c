4c737e2f75e754d360216c796b68ca6b7866b97c
mnrx2020 <jamesmbere@gmail.com>
2022-03-16 12:21:22 -0700

    Multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,17 +1,16 @@
-cat > 0-main.c
 #include "main.h"
-
 /**
  * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
+
 {
 	int i;
 
 	i = 0;
-	positive_or_negative(int i);
+	positive_or_negative(i);
 
-	 return (0);
+	return (0);
 }
